2d02d945bcd397540554a2576e932ffddc3dc69a
NokoDev <demozflava@gmail.com>
2022-08-15 18:54:14 +0200

    add new two files for adding a new task

---
+++ src/Components/InputForm.js
@@ -0,0 +1,13 @@
+
+
+const InputForm = () =>{
+
+    return (
+        <div>
+            <input type="text" />
+            <button>ADD</button>
+        </div>
+    )
+}
+
+export default InputForm;
