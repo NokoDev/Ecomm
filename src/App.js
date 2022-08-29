f3a8b4047f8bcef7f9d12031134b3dd737ab76ae
NokoDev <demozflava@gmail.com>
2022-08-29 16:29:54 +0200

    created dummy task array and passed to the Tasklist component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import classes from './App.module.css';
 import Titles from './Components/Titles';
 import TaskList from './Components/TaskList';
 
+const tasks = [
+  {key: "001",name: "Eat",description: "xxx",time: Date.now(),status: "added"},
+  {key: "002",name: "Sleep",description: "xxx",time: Date.now(),status: "In-progress"}
+]
 
 const App = () => {
   return (
     <div className={classes.main}>
       
       <Titles />
-      <TaskList />
+      <TaskList items={tasks}/>
     </div>
   )
 }
