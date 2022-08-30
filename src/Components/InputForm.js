287e2336def6c92c9fbcfa9299c8c563385c939d
NokoDev <demozflava@gmail.com>
2022-08-30 08:59:25 +0200

    creating a form overlay to add task

--- src/Components/InputForm.js
+++ src/Components/InputForm.js
@@ -4,6 +4,17 @@ const InputForm = () =>{
 
     return (
         <div className={classes.main}>
+            <label>Name</label>
+            <input type="text" className={classes.input}/>
+
+            <label>Description</label>
+            <input type="text" className={classes.input}/>
+
+            <label>Priority</label>
+            <input type="text" className={classes.input}/>
+
+
+            <label>Status</label>
             <input type="text" className={classes.input}/>
             <button className={classes.button}>ADD</button>
         </div>
