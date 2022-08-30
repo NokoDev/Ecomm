3843918dba4f6368805ee79e7095a75f2f8ed0b7
NokoDev <demozflava@gmail.com>
2022-08-30 09:01:30 +0200

    creating re-useable components

---
+++ src/UI/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+import classes from "Card.module.css"
+
+
+const Card = props =>{
+
+    return (
+        <div className={classes.main}>
+
+        </div>
+    )
+}
+
+export default Card
